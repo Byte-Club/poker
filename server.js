f0e1cd0d6fdee87fea6d5702b310a2098ddceaa8
Raymond K. Hessel <raymondkhessel2@gmail.com>
2012-03-09 17:35:57 -0800

    cleanup server.js

--- server.js
+++ server.js
@@ -1,43 +1,35 @@
-var app = require('express').createServer(),
-    io = require('socket.io').listen(app);
-
+var path = require('path'),
+app = require('express').createServer();
 app.listen(4021);
 
-// app.get('*/', function(req, res) {
-//   res.sendfile(__dirname + "/site/index.html");
-// });
+app.get('/', function(req, res) {
+	console.log(req);
+	res.send('hello world');
+});
 
 //route all asset requests to the assets folder:
 app.get('/assets/*', function(req, res) {
-  res.sendfile(__dirname + "/site/assets/" +req.params[0]);
-});
-app.get('/vendor/*', function(req, res) {
-  res.sendfile(__dirname + "/site/vendor/" +req.params[0]);
+	res.sendfile(__dirname + "/site/assets/" + req.params[0]);
 });
 
-
-//route everything else to the index ( to bootstrap the app)
-app.get('*', function(req, res) {
-  res.sendfile(__dirname + "/site/index.html");
+//route / to the index ( to bootstrap the app)
+app.get('/', function(req, res) {
+	res.sendfile(__dirname + "/site/index.html");
 });
 
-// //UNUSED - more specific file matching:
-// var requestURI = req.params[0];
-// if(!requestURI.match(/\.[^\/]+$/) && !requestURI.match(/\/$/)) { requestURI += '/'; }
-// if(requestURI.match(/\/$/)){ requestURI += 'index.html'; }
-// res.sendfile(__dirname + "/site" +requestURI);
+// handle all other GET requests
+webServer.get('/:filename.:format?', function(request, response) {
+	var fmt = request.params.format || 'html',
+	//default to html format
+	file = __dirname + '/site/' + request.params.filename + '.' + fmt;
 
+	//if the file exists, send it, otherwise 404
+	path.exists(file, function(exists) {
+		if (exists) {
+			response.sendfile(file);
+		} else {
+			response.send(404);
+		}
+	});
+});
 
-/**
- * Socket I/O
- */
-// var appFile = new App.FileJSON("images", function(json) {
-//   console.log(json);
-//   io.sockets.emit('input', json);
-// });
-
-// io.sockets.on('connection', function(socket) {
-//   socket.emit('input', appFile.get('json'));
-// });
-
-// require('./server/main');
